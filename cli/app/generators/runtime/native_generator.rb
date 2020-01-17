152fe2090adb374db829d1dd1e444b775739e7f8
Robert Roach <rjayroach@gmail.com>
2020-01-17 18:14:45 +0800

    first pass

--- cli/app/generators/runtime/native_generator.rb
+++ cli/app/generators/runtime/native_generator.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class Runtime::NativeGenerator < RuntimeGenerator
+
+  def generate_project_files
+    directory('files', target.write_path(:deployment))
+
+    Dir.chdir(target.write_path(:deployment).join('libexec')) do
+      map.each_pair do |src, dest|
+        FileUtils.ln_s(dest, src.to_s)
+      end
+    end
+
+    template('Procfile.infra.erb', "#{target.write_path(:deployment)}/Procfile.infra")
+    template('Procfile.erb', "#{target.write_path(:deployment)}/Procfile")
+  end
+
+  private
+
+  def map; { _kafka: '_docker', _localstack: '_docker', _sidekiq: '_rails', _spring: '_rails' } end
 end
--- cli/app/models/runtime/native.rb
+++ cli/app/models/runtime/native.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Runtime::Native < Runtime
+  def before_execute_on_target; switch! end
+
+
+  def switch!
+    response.output.puts 'switch! to be implemented'
+  end
+
+  def start
+    response.output.puts 'start to be implemented'
+  end
 end
