c22068c93cb64ae5598f9c6be2785c8f264b907f
Peter Hoeg <peter@hoeg.com>
2020-01-20 14:01:16 +0800

    native generator

--- cli/app/generators/runtime/native_generator.rb
+++ cli/app/generators/runtime/native_generator.rb
@@ -1,21 +1,77 @@
 # frozen_string_literal: true
 
+# Runtime for running native processes on host
 class Runtime::NativeGenerator < RuntimeGenerator
-
   def generate_project_files
-    directory('files', target.write_path(:deployment))
+    outdir = target.write_path(:deployment)
 
-    Dir.chdir(target.write_path(:deployment).join('libexec')) do
-      map.each_pair do |src, dest|
+    directory('files', outdir)
+
+    Dir.chdir(outdir.join('libexec')) do
+      symlink_map.each_pair do |src, dest|
         FileUtils.ln_s(dest, src.to_s)
       end
     end
 
-    template('Procfile.infra.erb', "#{target.write_path(:deployment)}/Procfile.infra")
-    template('Procfile.erb', "#{target.write_path(:deployment)}/Procfile")
+    template('router.conf.erb', "#{outdir}/router.conf")
+    template('Procfile.infra.erb', "#{outdir}/Procfile.infra")
+    template('Procfile.erb', "#{outdir}/Procfile")
   end
 
   private
 
-  def map; { _kafka: '_docker', _localstack: '_docker', _sidekiq: '_rails', _spring: '_rails' } end
+  def service_path(service)
+    service&.config[:is_cnfs_service] ? '$CORE_PATH' : '$PLATFORM_PATH'
+  end
+
+  def service_profiles(service)
+    (service.profiles || []) << 'spring'
+  end
+
+  def procfile_entry(service, profile, port = nil)
+    name = service.name
+
+    port = nil unless profile == 'server'
+
+    [
+      [name, profile].join('_') + ':',
+      server_name_map[profile.to_sym],
+      name,
+      service_path(service),
+      port
+    ].compact.join ' '
+  end
+
+  def conditional_infra_services_map
+    {
+      postgres: '_postgres',
+      fluentd: 'bundle exec fluentd --config ./fluent.conf',
+      kafka: '_kafka',
+      mail: '_mailcatcher',
+      redis: '_redis'
+    }
+  end
+
+  def infra_services_map
+    {
+      web: 'haproxy -- ./router.conf',
+      yard: 'yard server --gems'
+    }
+  end
+
+  def symlink_map
+    { _kafka: '_docker', _localstack: '_docker', _sidekiq: '_rails', _spring: '_rails' }.freeze
+  end
+
+  def server_name_map
+    { server: '_rails', worker: '_sidekiq', scheduler: '_scheduler', sqs_worker: '_sqs' }.freeze
+  end
+
+  def web_port
+    5000
+  end
+
+  def web_port_increment
+    10
+  end
 end
