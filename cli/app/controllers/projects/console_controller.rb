bb11f724e827258dc052f6fa0b33c440447d5a89
Robert Roach <rjayroach@gmail.com>
2020-11-23 23:21:24 +0800

    improve robustness of interactive console

--- cli/app/controllers/projects/console_controller.rb
+++ cli/app/controllers/projects/console_controller.rb
@@ -1,19 +1,61 @@
 # frozen_string_literal: true
 
+class Console < Pry::ClassCommand
+  match 'commands'
+  group 'cnfs'
+  description 'List commands available in the current command set'
+
+  def process(_command_set)
+    if target_self.class.name.eql?('Projects::ConsoleController')
+      puts "#{target_self.class.commands.join("\n")}\n\nreload!"
+    else
+      puts target_self.class.instance_methods(false).join("\n")
+    end
+  end
+end
+
+CnfsCommandSet = Pry::CommandSet.new
+CnfsCommandSet.add_command(Console)
+Pry.config.commands.import CnfsCommandSet
+
 module Projects
   class ConsoleController
     include ExecHelper
 
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
     def execute
       require 'pry'
-      Pry.start(self, prompt: proc { |_obj, _nest_level, _| 'cnfs> ' })
+      project = Pry::Helpers::Text.blue(Cnfs.project.name)
+      environment_name = Cnfs.project.environment.name
+      environment_color = environment_name.eql?('production') ? 'red' : 'green'
+      environment = Pry::Helpers::Text.send(environment_color, environment_name)
+      prompt = proc do |obj, _nest_level, _|
+        "[#{project}][#{environment}] " \
+          "(#{Pry.view_clip(obj.class.name.demodulize.delete_suffix('Controller').underscore).gsub('"', '')}) > "
+      end
+      Pry.config.prompt = Pry::Prompt.new('cnfs', 'cnfs prompt', [prompt])
+      Pry.start(self)
     end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
 
     class << self
       def shortcuts
         return {} unless defined?(ActiveRecord)
 
         { b: Blueprint, e: Environment, k: Key, n: Namespace, p: Provider, r: Repository, s: Service, u: User }
+      end
+
+      def commands
+        %i[projects repositories environments namespaces images services]
+      end
+    end
+
+    commands.each do |command_set|
+      define_method(command_set) do
+        Pry.start("#{command_set}_controller".classify.safe_constantize.new(args, options))
+        true
       end
     end
 
@@ -40,9 +82,13 @@ module Projects
       true
     end
 
-    def r; reload! end
+    def r
+      reload!
+    end
 
-    def m; project.manifest end
+    def m
+      project.manifest
+    end
 
     def o
       options
@@ -55,17 +101,6 @@ module Projects
     def od(key)
       @options = Thor::CoreExt::HashWithIndifferentAccess.new(options.except(key.to_s))
       options
-    end
-
-    def cmd
-      OpenStruct.new({
-        projects: ProjectsController.new(args, options),
-        repositories: RepositoriesController.new(args, options),
-        environments: EnvironmentsController.new(args, options),
-        namespaces: NamespacesController.new(args, options),
-        images: ImagesController.new(args, options),
-        services: ServicesController.new(args, options)
-      })
     end
   end
 end
