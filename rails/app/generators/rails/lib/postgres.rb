cbeaf265153a0658704cad2b3c83dd853d95b5bc
Robert Roach <rjayroach@gmail.com>
2021-11-25 09:41:48 +0800

    remove rails gem in favor of repository component

--- rails/app/generators/rails/lib/postgres.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# postgres.rb
-
-remove_file("#{cnfs.app_dir}/config/database.yml")
-@database_prefix = cnfs.name.tr('-', '_').to_s
-template('config/database.yml', "#{cnfs.app_dir}/config/database.yml")
--- rails/lib/cnfs_cli/rails/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module CnfsCli
-  module Rails
-    VERSION = '0.1.0'
-  end
-end
